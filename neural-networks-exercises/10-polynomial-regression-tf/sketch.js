c9a28b030d5bd480fb1521fbca17eaaf87ee62b5
michaelruppe <michael.ruppe@live.com.au>
2019-04-05 17:49:17 +1100

    update 10-polynomial-regression-tf

--- neural-networks-exercises/10-polynomial-regression-tf/sketch.js
+++ neural-networks-exercises/10-polynomial-regression-tf/sketch.js
@@ -1,41 +1,48 @@
 // Perform polynomial regression on points dropped by user with mouse clicks
-
+// a natural upgrade from the previous exercise
 // TODO: vector of coefficients a0, a1, a2 etc. at the moment each coeff is managed manually = yuck
 
 // arrays for data points
 let x_vals = [];
 let y_vals = [];
 
-let m, b, a, c;
+let a, b, c, d;
+
+let dragging = false;
 
 // Set parameters
 const learningRate = 0.4;
 const optimizer = tf.train.adam(learningRate);
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(800,800);
   background(0);
 
   // initialising the parameters to approximate, make sure the values are updateable
-  m = tf.variable( tf.scalar(random(1)) );
-  b = tf.variable( tf.scalar(random(1)) );
   a = tf.variable( tf.scalar(random(1)) );
+  b = tf.variable( tf.scalar(random(1)) );
   c = tf.variable( tf.scalar(random(1)) );
+  d = tf.variable( tf.scalar(random(1)) );
 
 
 }
 
 function draw() {
+  if(dragging) {
+    x_vals.push( map(mouseX, 0, width, -1, 1) );
+    y_vals.push( map(mouseY, 0, height, 1, -1) );
+  } else {
+    // can't do these when no data!
+    if (x_vals.length > 0) {
+      tf.tidy(() => {
+        const ys = tf.tensor1d(y_vals);
 
-  // can't do these when no data!
-  if (x_vals.length > 0) {
-    tf.tidy(() => {
-      const ys = tf.tensor1d(y_vals);
+        // minimizes output. optional 3rd argument var list sets which variables are trainable. since none provided, assumes all trainable
+        // it is optional to train only selected coefficients
+        optimizer.minimize(() => loss(predict(x_vals), ys));
+      });
 
-      // minimizes output. optional 3rd argument var list sets which variables are trainable. since none provided, assumes all trainable
-      // to train only m or only b you have to specify
-      optimizer.minimize(() => loss(predict(x_vals), ys));
-    });
+    }
   }
 
 
@@ -71,10 +78,12 @@ function draw() {
     }
 }
 
-// Add the clicked point to our data points
+// Flag mouse activity
 function mousePressed() {
-  x_vals.push( map(mouseX, 0, width, -1, 1) );
-  y_vals.push( map(mouseY, 0, height, 1, -1) );
+  dragging = true;
+}
+function mouseReleased() {
+  dragging = false;
 }
 
 // in ES6
@@ -86,9 +95,11 @@ function loss(pred, label) {
 function predict(x) {
   const xs = tf.tensor1d(x);
 
-  // y = ax^2 + bx + c
-  const ys = xs.square().mul(a).add(
-    xs.mul(b).add(c)
+  // y = ax^3 + bx^2 + cx + d
+  const ys = xs.pow(3).mul(a).add(
+    xs.square().mul(b).add(
+      xs.mul(c).add(d)
+    )
   );
   return ys;
 }
